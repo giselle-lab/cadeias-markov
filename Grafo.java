e58df07482d660a8ec96817c4f686a6ff48cb197
Giselle Pereira <56452652+giselle-lab@users.noreply.github.com>
2022-03-09 17:43:36 -0300

    Add files via upload

--- Grafo.java
+++ Grafo.java
@@ -1,20 +1,25 @@
 import java.util.*;
  
 //class to store edges of the weighted graph
-class Edge {
-    int src, dest, weight;
-    Edge(int src, int dest, int weight) {
+class Edge extends Jogador{
+    int src, dest;
+    double weight;
+    //String name;
+    Edge(int src, int dest, double weight) {
             this.src = src;
             this.dest = dest;
             this.weight = weight;
+
         }
 }
 // Graph class
-class Graph {
+class Graph extends Jogador{
     // node of adjacency list 
     static class Node {
-        int value, weight;
-        Node(int value, int weight)  {
+        int value;
+        double weight;
+        String name;
+        Node(int value, double weight)  {
             this.value = value;
             this.weight = weight;
         }
@@ -55,13 +60,26 @@ List<List<Node>> adj_list = new ArrayList<>();
             src_vertex++;
         }
     }
+    public double getRandomNumber(double min, double max) {
+        return (double) ((Math.random() * (max - min)) + min);
+    }
 }
+
+
+
 class Main{
     public static void main (String[] args) {
         // define edges of the graph 
-        List<Edge> edges = Arrays.asList(new Edge(0, 1, 2),new Edge(0, 2, 4),
-                   new Edge(1, 2, 4),new Edge(2, 0, 5), new Edge(2, 1, 4),
-                   new Edge(3, 2, 3), new Edge(4, 5, 1),new Edge(5, 4, 3));
+        double min= 0.7, max = 0.8;
+        double p = (double) Math.floor(Math.random()*(max-min+1)+min);
+        double q = 1-p;
+        Jogador jA = new Jogador("ja",p);
+        Jogador jB = new Jogador("jb",q);
+        Jogo jogo = new Jogo(jA,jB);
+
+        List<Edge> edges = Arrays.asList(new Edge(0, 1,1-p),new Edge(0, 2,p),
+                    new Edge(1, 3, ),new Edge(1, 4, p), new Edge(2, 5, p),
+                   new Edge(2, , 3), new Edge(4, 5, 1),new Edge(5, 4, 3));
  
         // call graph class Constructor to construct a graph
         Graph graph = new Graph(edges);
